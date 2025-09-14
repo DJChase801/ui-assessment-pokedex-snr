10599089d1b1888041a0d52f24c818b954b1f232
DJChase801 <danchase801@gmail.com>
2025-09-14 14:46:29 -0600

    Get app in working state

--- src/hooks/useGetPokemons.ts
+++ src/hooks/useGetPokemons.ts
@@ -1,5 +1,5 @@
 import { useMemo } from 'react';
-import { useQuery } from '@apollo/react-hooks';
+import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 
 export type Pokemon = {
